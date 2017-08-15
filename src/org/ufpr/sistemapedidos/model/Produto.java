53be4968bf668cf4032af6fda4e34ef010f1b7bd
Roberto Duessmann <roberto.duessmann@gmail.com>
2017-08-15 13:19:33 -0300

    Create product model

---
+++ src/org/ufpr/sistemapedidos/model/Produto.java
@@ -0,0 +1,28 @@
+package org.ufpr.sistemapedidos.model;
+
+public class Produto {
+	
+	private int id;
+	private String descricao;
+	
+	public Produto(int id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
